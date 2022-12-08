1e5aae12b8b8058557822a5cc637cafcc515de39
h1teshtr1path1 <hiteshtripathi12345678@gmail.com>
2022-12-08 15:29:49 +0530

    initial tool for test is ready

--- src/transfer_frontend/src/index.jsx
+++ src/transfer_frontend/src/index.jsx
@@ -7,53 +7,53 @@ import { Audio } from 'react-loader-spinner'
 import PlugConnect from '@psychedelic/plug-connect';
 import { Principal } from '@dfinity/principal';
 
+import ext721Did from "../idls/ext721.did";
+
 
 import { getAccountId, getTokenIdentifier } from '../components/utils';
-import ext721 from '../idls/ext721.did'
+
+import {
+  getCachedUserNFTs,
+  getNFTActor,
+  NFTCollection,
+  NFTDetails,
+  getTokenActor,
+  TokenInterfaces,
+  getAddresses,
+  addAddress,
+  removeAddress,
+  getAllUserNFTs,
+  FungibleMetadata,
+} from '@psychedelic/dab-js';
 
 const App = () => {
 
   const [collectionCanister, setCollectionCanister] = useState("");
-  const [princiapals, setPrincipals] = useState("");
+  const [princiapals, setPrincipals] = useState([]);
   const [connect, setConnect] = useState("Please Connect you Wallet!");
   const [loader, setLoader] = useState(false);
-  const [tokenIndex, setTokenIndex] = useState(0);
+  const [tokenIndex, setTokenIndex] = useState([]);
   const [from, setFrom] = useState("");
 
   //plug connection and method call
   const CollectionCanisterId = "rw7qm-eiaaa-aaaak-aaiqq-cai";
-  const whitelist = [CollectionCanisterId];
+  const whitelist = ["rw7qm-eiaaa-aaaak-aaiqq-cai", "gikg4-eaaaa-aaaam-qaieq-cai"];
 
-  //transfer request
-  // const TransferRequest = IDL.Record({
-  //   'to' : User,
-  //   'token' : TokenIdentifier,
-  //   'notify' : IDL.Bool,
-  //   'from' : User,
-  //   'memo' : Memo,
-  //   'subaccount' : IDL.Opt(SubAccount),
-  //   'amount' : Balance,
-  // });
 
-  const sample_batch = {
-    idl: ext721,
-    canisterId: CollectionCanisterId,
-    methodName: 'transfer',
-    args: [Principal.fromText(princiapals),
-    getTokenIdentifier(CollectionCanisterId, tokenIndex),
-    false,
-    Principal.fromText(from),
-    [],
-    null,
-    Number(1)
-    ],
-    onSuccess: async (res) => {
-      alert('transferred xtc successfully');
-    },
-    onFail: (res) => {
-      alert('transfer xtc error', res);
-    },
-  };
+  const handleCollectionChange = async (event) => {
+    setCollectionCanister(event.target.value);
+    // console.log(collectionCanister)
+  }
+  const handlePrincipalChange = async (event) => {
+    const str = event.target.value;
+    setPrincipals(str.split(','));
+    // console.log(princiapals);
+  }
+
+  const handleIndexChange = async (event) => {
+    const str = event.target.value;
+    setTokenIndex(str.split(','));
+  }
 
   const handleTransfer = async (event) => {
     const isConnected = await window.ic.plug.isConnected();
@@ -63,27 +63,65 @@ const App = () => {
     }
     const collectionActor = await window.ic.plug.createActor({
       canisterId: CollectionCanisterId,
-      interfaceFactory: collectionCanisterIDL,
+      interfaceFactory: ext721Did,
     });
 
     try {
       setLoader(true)
       const sessionData = window.ic.plug.sessionManager.sessionData;
       const to = princiapals;
+      const indices = tokenIndex;
       const f = sessionData.principalId;
+      const dummyMemmo = new Array(32).fill(0);
       setFrom(f);
-      const transactions = [sample_batch];
-      await window.ic.plug.batchTransactions(transactions)
+
+      const txs = [];
+      for(let i = 0; i< to.length; i++){
+        const tx = {
+          idl: ext721Did,
+          canisterId: collectionCanister,
+          methodName: 'transfer',
+          args: [{
+            to: { principal: Principal.fromText(to[i])},
+            from: { principal: Principal.fromText(f)},
+            token: getTokenIdentifier(collectionCanister, Number(indices[i])),
+            amount: BigInt(1),
+            memo: new Array(32).fill(0),
+            notify: false,
+            subaccount: [],
+          }],
+          onSuccess: async (res) => {
+            console.log('transfer success!');
+          },
+          onFail: (res) => {
+            console.log('transfer error', res);
+          },
+        };
+        txs.push(tx);
+      }
+
+      await window.ic.plug.batchTransactions(txs)
       setLoader(false)
     }
     catch (err) {
-      alert(err);
+      console.log(err);
       setLoader(false)
     }
   };
 
+  const getNFTCollections = async (event) => {
+    const principal = from;
+    const agent = window.ic.plug.agent;
+    const sessionData = window.ic.plug.sessionManager.sessionData;
+    const f = sessionData.principalId;
+    const collections = await getAllUserNFTs({
+      agent,
+      user: f
+    });
+    console.log(JSON.stringify(collections, (_, v) => typeof v === 'bigint' ? v.toString() : v));
+  }
+
   const ConnectCallback = async (event) => {
-    setWallet(window.ic.plug.principalId);
     const isConnected = await window.ic.plug.isConnected();
     if (isConnected) {
       setConnect("Connected!");
@@ -126,6 +164,7 @@ const App = () => {
           </div>
           <div>
             <PlugConnect
+              dark='true'
               whitelist={whitelist}
               onConnectCallback={ConnectCallback}
             />
@@ -138,7 +177,7 @@ const App = () => {
             name="Collection"
             placeholder="Collection Canister ID?"
             required
-            onChange={(event) => setCollectionCanister(event.target.value)}
+            onInput={handleCollectionChange}
             value={collectionCanister}
           ></input>
           </div>
@@ -146,8 +185,16 @@ const App = () => {
             name="principals"
             placeholder="principal ids?"
             required
-            onChange={(event) => setPrincipals(event.target.value)}
+            onInput={handlePrincipalChange}
             value={princiapals}
+          ></input>
+          </div>
+          <div><input
+            name="indices"
+            placeholder="Nft indices? (, separated)?"
+            required
+            onInput={handleIndexChange}
+            value={tokenIndex}
           ></input>
           </div>
           <button
@@ -157,6 +204,17 @@ const App = () => {
             Transfer
           </button>
         </div>
+      </div>
+      <br></br>
+      <br></br>
+      <div>
+        <button
+          style={{ backgroundColor: "transparent", cursor: 'pointer', marginTop: 20, marginBottom: 20, width: 200, height: 50 }}
+          className=""
+          onClick={getNFTCollections}>
+          Check NFT Balance?
+        </button>
+        {/* <div>{balance}</div> */}
       </div>
     </div>
   );
